6cb0d0deb56f37a855ca03e1426d0ee47ce77a92
Taina Adrielly] <martins.taina@aluno.ifsp.edu.br>
2026-05-27 08:35:53 -0300

    crindo tabelas

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import express from 'express'
+import cors from 'cors'
+import bodyParser from 'body-parser'
+
+const server = express()
+
+server.use(cors())
+server.use(bodyParser.json())
+server.use(bodyParser.urlencoded({ extended:true}))
+server.use(express.static('public'))
+
+server.get('api/teste', (req, res) => {
+    res.json({ message: 'API funcionando!' })
+})
