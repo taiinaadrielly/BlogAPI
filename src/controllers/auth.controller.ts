830c70f3916fe300fedecf611fa063a340404ad7
Taina Adrielly] <martins.taina@aluno.ifsp.edu.br>
2026-06-08 08:39:33 -0300

    Criar autenticacao JWT para login e registro de usuarios

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,39 +1,43 @@
 import { RequestHandler } from "express"
 import { z } from "zod"
 import { createUser } from "../services/user.service"
+import { createUser, verifyUser } from "../services/user.service"
+import { createToken } from "../services/auth.service"
 
 export const signIn: RequestHandler = (req, res) => {
 	// Lógica para autenticar o usuário
-}
-
-export const signUp: RequestHandler = (req, res) => {
+export const signIn: RequestHandler = async (req, res) => {
 	const schema = z.object({
-		name: z.string(),
 		email: z.string().email(),
 		password: z.string()
 	})
 	const data = schema.safeParse(req.body)
 	if (!data.success) {
-		return res.status(400).json({ error: data.error.flatten().fieldErrors })
+		return res.status(400).json({ error: data.error.flatten().fieldErrors})
 	}
-	const newUser = await createUser(data.data)
-	if (!newUser) {
-		return res.status(400).json({ error: 'Email já cadastrado' })
+	const user = await verifyUser(data.data)
+	if (!user) {
+		return res.status(400).json({ error: 'Email ou senha inválidos'})
 	}
-
-	const token = '123'
-	res.status(201).json({
-		message: 'Usuario criado com sucesso',
+	const token = createToken(user)
+	res.json({
+		message: 'Login bem-sucedido',
 		user: {
-			id: newUser.id,
-			name: newUser.name,
-			email: newUser.email
+			id: user.id,
+			name: user.name,
+			email: user.email
 		},
 		token
 	})
-
 }
 
-export const validate: RequestHandler = (req, res) => {
-	// Lógica para validar o token de autenticação
+export const signUp: RequestHandler = async (req, res) => {
+@@ -21,7 +43,7 @@ export const signUp: RequestHandler = async (req, res) => {
+return res.status(400).json({ error: 'Email já cadastrado'})
 }
+
+	const token = '123'
+	const token = createToken(newUser)
+res.status(201).json({
+message: 'Usuário criado com sucesso',
+user: {
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -7,14 +7,19 @@ type CreateUserProps = {
     password: string
 }
 
-export const createUser = async ({ name, email, password }: CreateUserProps ) => {
+type verifyUserProps = {
+    email: string
+    password: string
+}
+
+export const createUser = async ({ name, email, password }: CreateUserProps) => {
     email = email.toLowerCase()
 
     const user = await prisma.user.findFirst({
         where: { email }
     })
 
-    if(user) return false
+    if (user) return false
 
     const hashedPassword = await bcrypt.hash(password, 10)
 
@@ -22,7 +27,20 @@ export const createUser = async ({ name, email, password }: CreateUserProps ) =>
         data: {
             name,
             email,
-            password: hashedPassword 
+            password: hashedPassword
         }
     })
+}
+
+export const verifyUser = async ({ email, password }: VerifyUserProps) => {
+    email = email.toLowerCase()
+
+    const user = await prisma.user.findFirst({
+        where: { email }
+    })
+    if (!user) return false
+    const isMatch = await bcrypt.compareSync(password, user.password)
+    if (!isMatch) return false
+
+    return user
 }
